858bfaa1639d0dcd59e487be4a502cae68b93b93
Georgi Yunchov <georgiiunchov@gmail.com>
2023-02-14 17:59:13 +0200

    Solved Tasks
    Solved problems from advanced functions lab

---
+++ advancedFunctionsLab/commandProcessor.js
@@ -0,0 +1,29 @@
+function solution() {
+    let result = '';
+
+    return {
+        append(str) {
+            result += str;
+        },
+
+        removeStart(num) {
+            result = result.slice(num);
+        },
+
+        removeEnd(num) {
+            result = result.slice(0, -num);
+        },
+
+        print() {
+            console.log(result);
+        },
+    };
+}
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
